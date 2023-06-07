ce77904dd5b4befdc7a444cedaae858d162db3b3
Jean-Louis Macle <jeanlouis.macle@gmail.com>
2023-06-07 13:41:36 -0500

    Code

--- jl_ml_utils/data_cleaning_and_processing.py
+++ jl_ml_utils/data_cleaning_and_processing.py
@@ -11,18 +11,22 @@ class DataCleaningAndProcessing:
     def import_csv_to_numpy_array(self, path_to_csv_file, skip_header_option):
         return np.genfromtxt(path_to_csv_file, delimiter=',' , skip_header=skip_header_option)    
     
-    def import_csv_to_df(self, path_to_csv_file, has_low_memory_option):
-        return pd.read_csv(path_to_csv_file, low_memory=has_low_memory_option)
+    def import_csv_to_df(self, path_to_csv_file, low_memory_setting):
+        return pd.read_csv(path_to_csv_file, low_memory=low_memory_setting)
 
 # 2. Data encoding fixing 
    
     
 # 3. Row removals
-    def remove_rows_with_data_unavailable_from_csv_file(self, path_to_csv_folder, csv_name):
-        data_frame = pd.read_csv(os.path.join(path_to_csv_folder, csv_name))
+    def drop_duplicates_and_remove_rows_with_data_unavailable_from_csv_file(self, path_to_csv_folder, csv_name, low_memory_setting):
+        #  low_memory parameter added to fix the following warning:
+        #  DtypeWarning: Columns (76,89) have mixed types. Specify dtype option on import or set low_memory=False.
+        data_frame = pd.read_csv(os.path.join(path_to_csv_folder, csv_name), low_memory=low_memory_setting)
+
+        data_frame = data_frame.drop_duplicates()
         data_frame = data_frame.dropna()
         # Creation of cleaned file name from original csv file name
-        cleaned_csv_name = csv_name.split('.')[0] + "_na_removed.csv"
+        cleaned_csv_name = csv_name.split('.')[0] + "_na_and_duplicates_removed.csv"
         data_frame.to_csv(os.path.join(path_to_csv_folder,cleaned_csv_name),index=False)
 
     def remove_rows_with_commas_only(self, path_to_csv_folder, csv_name, number_of_commas_in_empty_row):
@@ -32,9 +36,11 @@ class DataCleaningAndProcessing:
                 for line in file:
                     if (number_of_commas_in_empty_row*',') not in line:
                         output_file.write(line)
+        return cleaned_csv_name
 
-# 4.Duplicates removals
-    def pre_pivot_table_pre_cleaning(df, column1, column2, print_data_for_unique_values):
+# 4.
+    def pre_pivot_table_pre_cleaning(self, df, column1, column2, print_data_for_unique_values):
+      results_dict = {}
       column1_series = df[column1]
       column1_series_unique_values = column1_series.unique()
       if print_data_for_unique_values:
@@ -51,9 +57,28 @@ class DataCleaningAndProcessing:
             if len(potential_values) > 1:
                   # List all line numbers for a given value
                   for value in potential_values:
-                        line_numbers_count = sum(coresponding_column2_data == value)
-                        print(f"Line numbers count for {column2} {value}: {line_numbers_count}")
-
+                        line_numbers_count = sum(coresponding_column2_data == value)                        
+                        # Adding to a list the line numbers for a given value
+                        results_dict[value] = line_numbers_count
+            # Sorting the dictionary to list the values with the least occurrences first
+            
+      results_dict = dict(sorted(results_dict.items(), key=lambda item: item[1], reverse=False))
+      return results_dict
+    
+    def line_finding_given_labels_and_column(self, df, column, labels_list):
+        results = {}
+        for label in labels_list:            
+                mask = df[column] == label
+                df_for_lines = df[mask]
+                line_numbers = df_for_lines.index.tolist()  
+                # numbers need to be incremented by 2 
+                # because the first line is the header 
+                # and the indexation starts from 0
+                line_numbers_updated = [x + 2 for x in line_numbers]
+                results[label] = line_numbers_updated
+        print(f"Lines numbers where {labels_list} can be found :")
+        print(results)
+        return results
 
 
 # Encoding issues encountered :
