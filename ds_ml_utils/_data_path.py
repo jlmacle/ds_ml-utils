4cc1c165b84e67aa7efe76f1f14d32c32a21eaac
Jean-Louis Macle <jeanlouis.macle@gmail.com>
2023-06-12 14:27:34 -0500

    Code cleanup

--- ds_ml_utils/_data_path.py
+++ ds_ml_utils/_data_path.py
@@ -5,6 +5,7 @@ class DataPath:
         self.path_to_csv_folder = "Undefined"
         self.csv_name_original = "Undefined"
         self.csv_name_cleaned = "Undefined"
+        self.path_to_original_data = "Undefined"
         self.path_to_cleaned_data = "Undefined"
         self.path_to_folder_with_data_for_reporting = "Undefined"
         self.csv_report_file_name = "Undefined"
@@ -16,7 +17,10 @@ class DataPath:
     def get_file_name_for_csv_with_original_data(self):
         return self.csv_name_original
     
-    def get_file_name_for_csv_with_cleaned_data(self):  
+    def get_path_to_original_csv_file(self):
+        return os.path.join(self.path_to_csv_folder, self.csv_name_original)
+    
+    def get_file_name_for_csv_with_cleaned_data(self):          
         return self.csv_name_cleaned
     
     def get_path_to_cleaned_csv_file(self):
@@ -45,9 +49,6 @@ class DataPath:
 
     def set_file_name_for_csv_with_cleaned_data(self, csv_name_cleaned):
         self.csv_name_cleaned = csv_name_cleaned
-
-    def set_path_to_cleaned_csv_file(self, path_to_cleaned_data):
-        self.path_to_cleaned_data = path_to_cleaned_data
 
     def set_path_to_folder_with_data_for_reporting(self, path_to_folder_with_data_for_reporting):
         self.path_to_folder_with_data_for_reporting = path_to_folder_with_data_for_reporting
